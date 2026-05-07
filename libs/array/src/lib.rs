40ace5e4da71f0a01d2f6dfa082c69ec2cc52a8e
pducos <paulsaksela@gmail.com>
2026-05-07 17:02:59 +0200

    add credits to nils

--- libs/array/src/lib.rs
+++ libs/array/src/lib.rs
@@ -1,5 +1,8 @@
 #![no_std]
 
+// this was taken from https://github.com/nils-mathieu/ruel/blob/main/src/utility/array_vec.rs
+// give credits to the man
+
 pub mod array_string;
 pub mod array_vec;
 pub mod uninit_array;
